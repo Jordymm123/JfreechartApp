21c2a76b2e00506788433902cd7db67a4032fef0
Jordymm123 <141835719+Jordymm123@users.noreply.github.com>
2023-10-24 21:02:04 -0500

    Add files via upload

---
+++ TallerjFreeChart/src/tallerjfreechart/TallerjFreeChart.java
@@ -0,0 +1,21 @@
+package tallerjfreechart;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.DefaultComboBoxModel;
+import java.util.Arrays;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.data.general.DefaultPieDataset;
+public class TallerjFreeChart {
+    public static void main(String[] args) {
+        
+    }
+    
+}
